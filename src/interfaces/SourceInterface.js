ffa1c99a3683d39c7bb9070b7fc609fed6a98814
Mark <git@happybeing.com>
2020-01-23 15:47:52 +0000

    Implement creation of named interfaces (non-functional)

--- src/interfaces/SourceInterface.js
+++ src/interfaces/SourceInterface.js
@@ -1,22 +1,49 @@
 // Base class for managing a data source
 //
 // TODO: move subclasses to separate files and export from this file
+// TODO: remove all console.dir
 
 class SourceInterface {
-  constructor () {
+  constructor (shortName, description) {
     console.warn('SourceInterface - to be implemented');
-  // TODO: define base interface
+    this.shortName = shortName;
+    this.description = description;
   }
+  
+  // TODO: define base interface
 
-    // TODO: maintain named, id'd list of all SourceInterface objects for SourceUI
-    // TODO: start with four fixed interfaces: 
-    // TODO:  - RDF (fixed LDP access)
-    // TODO:  - File (a fixed file)
-    // TODO:  - Manual (fixed user)
-    // TODO:  - Generated (ngraph)
-    // TODO: add serialisation (file save/load and incorporate in app-wide project/template serialisation)
-    // TODO: add UI for create, copy and edit SourceInterface (and in each subclass UI component)
+  // TODO: maintain named, id'd list of all SourceInterface objects for SourceUI
+  // TODO: start with four fixed interfaces: 
+  // TODO:  - RDF (fixed LDP access)
+  // TODO:  - File (a fixed file)
+  // TODO:  - Manual (fixed user)
+  // TODO:  - Generated (ngraph)
+  // TODO: add serialisation (file save/load and incorporate in app-wide project/template serialisation)
+  // TODO: add UI for create, copy and edit SourceInterface (and in each subclass UI component)
 
+}
+
+export class SourceInterfaceManager {
+  constructor (interfaceDefinitions) {
+    console.warn('SourceInterfaceManager - to be implemented');
+
+     this.initialiseInterfaces(interfaceDefinitions !== undefined ? interfaceDefinitions : testInterfaces);
+  }
+    
+  ////////////////////////////////
+
+  initialiseInterfaces (interfaceDefinitions) {
+    console.log('SourceInterfaceManager.initialiseInterfaces()...');
+    this.sourceInterfaces = new Map;
+
+    interfaceDefinitions.forEach(def => {
+      try {
+        let newInterface = new def.iClass(def.shortName, def.description);
+        this.sourceInterfaces.set(def.shortName, newInterface);
+        console.dir(newInterface);
+      } catch(e) {console.warn(e);}
+    });
+  }
 }
 
 class SourceResult {
@@ -29,7 +56,8 @@ class SourceResult {
 
 // TODO: extract generic web stuff from RdfInterface
 class WebInterface extends SourceInterface {
-  constructor () {
+  constructor (shortName, description) {
+    super(shortName, description);
     console.warn('WebInterface - to be implemented');
   }
 }
@@ -37,23 +65,34 @@ class WebInterface extends SourceInterface {
 // TODO: LDP
 // TODO: SPARQL
 class RdfInterface extends SourceInterface {
-  constructor () {
+  constructor (shortName, description) {
+    super(shortName, description);
     console.warn('RdfInterface - to be implemented');
+  }
+}
+
+// TODO: JSON - initially just {nodes: [], links []}
+class JsonInterface extends SourceInterface {
+  constructor (shortName, description) {
+    super(shortName, description);
+    console.warn('JsonInterface - to be implemented');
   }
 }
 
 // TODO: file system interface
 class FileInterface extends SourceInterface {
-  constructor () {
+  constructor (shortName, description) {
+    super(shortName, description);
     console.warn('FileInterface - to be implemented');
   }
-  // TODO: NOW: just load a fixed local file system
+  // TODO: NOW: just load a fixed local file system, passes to RdfInterface or JSON interface based on extension
   // TODO: LATER: provide file selection with optional default file exenstions
 }
 
 // TODO: manual input interface
 class ManualInterface extends SourceInterface {
-  constructor () {
+  constructor (shortName, description) {
+    super(shortName, description);
     console.warn('ManualInterface - to be implemented');
   }
 
@@ -61,9 +100,20 @@ class ManualInterface extends SourceInterface {
 
 // TODO: auto-generator interface
 class GeneratorInterface extends ManualInterface {
-  constructor () {
+  constructor (shortName, description) {
+    super(shortName, description);
     console.warn('GeneratorInterface - to be implemented');
   }
-
-
 }
+
+// TODO: replace fixed interfaces with an initial set
+// TODO: change iClass to String and use a 'factory' so I can serialise (research ways to serialise first)
+const testInterfaces = [
+  // {className: "", shortName: "", description: "", options: {}},
+  {iClass: RdfInterface, shortName: "ldp-test", description: "LDP (RDF/Turtle)", options: {}},
+  {iClass: RdfInterface, shortName: "sparql-test", description: "SPARQL (RDF/Turtle) - TBD", options: {}},
+  {iClass: JsonInterface, shortName: "json-test", description: "File (JSON)", options: {}},
+  {iClass: ManualInterface, shortName:  "manual-test", description: "Manual (mrh)", options: {}},
+  {iClass: GeneratorInterface, shortName:  "generator-test", description: "Generator (mrh)", options: {}},
+  ];
+  
