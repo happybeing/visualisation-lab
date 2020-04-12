58ea86f45608b94ad33e76c93f0ce69ccdc08518
Mark <git@happybeing.com>
2020-04-12 21:07:55 +0100

    Add wikidata and europa.eu endpoints, add comments for some

--- src/data/dataSources.js
+++ src/data/dataSources.js
@@ -1,6 +1,18 @@
 export const dataSources =
 [
-  // { endpoint: 'https://dbpedia.org/sparql', options: {}},
-  { endpoint: 'https://data.carnegiehall.org/sparql', options: {}},
-  // { endpoint: 'http://os.rkbexplorer.com/sparql/', options: {}},
+  // { endpoint: '', options: {}},
+
+  { endpoint: 'https://dbpedia.org/sparql', options: {}},  // works
+  { endpoint: 'https://data.europa.eu/euodp/sparqlep', options: {}}, // works
+
+  // { endpoint: 'https://data.carnegiehall.org/sparql', options: {}},  // Only returns HTML
+  // { endpoint: 'http://os.rkbexplorer.com/sparql/', options: {}}, // CORS error for 'sparql-count'
+
+  { endpoint: 'https://query.wikidata.org/sparql', options: {}},
+
+
+  // { endpoint: '', options: {}},
+  // { endpoint: '', options: {}},
+  // { endpoint: '', options: {}},
+  // { endpoint: '', options: {}},
 ];
